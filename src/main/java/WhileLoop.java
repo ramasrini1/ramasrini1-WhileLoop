1cb304babb03e9d47ddcbf26b824c87aff4d91cd
Rama Srinivas <ramasrini1@gmail.com>
2023-02-01 18:07:04 +0000

    adding sum

--- src/main/java/WhileLoop.java
+++ src/main/java/WhileLoop.java
@@ -35,9 +35,10 @@ public class WhileLoop {
     public int whileExample(int end){
         int sum = 0;
         int i = 0;
-
-//        code here
-
+        while ( i < end ){
+            sum = sum + i;
+            i++;
+        }
         return sum;
     }
 }
